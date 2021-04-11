8bd07fa9193d7fa41c7a7d32dac4432b516957e8
mu2eswari <80945176+mu2eswari@users.noreply.github.com>
2021-04-11 09:07:06 +0530

    Add files via upload

---
+++ Week5/Day1/Classroom/BaseClass.java
@@ -0,0 +1,37 @@
+package Week5.Day1.Classroom;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+	public ChromeDriver driver;
+	
+	@BeforeClass
+	public void preConditon()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+	}
+	
+	@AfterClass
+	public void postCondition()
+	{
+		driver.close();
+	}
+
+}
---
+++ Week5/Day1/Classroom/CreateLead.java
@@ -0,0 +1,27 @@
+package Week5.Day1.Classroom;
+
+
+
+import org.openqa.selenium.By;
+
+import org.testng.annotations.Test;
+
+
+
+public class CreateLead extends BaseClass {
+
+	@Test
+	public void runcreateLead() {
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+	}
+}
+
+
+
+
+
+
